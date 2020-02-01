17b551baa4068d0083fd5ce045ae15f209e56733
Gaelmare <2gaelmare@gmail.com>
2020-02-01 16:56:54 -0700

    Update with different DT vanilla Species for different TFC trees

--- src/main/java/org/labellum/mc/dynamictreestfc/DTFCGenerator.java
+++ src/main/java/org/labellum/mc/dynamictreestfc/DTFCGenerator.java
@@ -2,26 +2,25 @@ package org.labellum.mc.dynamictreestfc;
 
 import java.util.Random;
 
-import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.gen.structure.template.TemplateManager;
 
-import com.ferreusveritas.dynamictrees.ModConstants;
-import com.ferreusveritas.dynamictrees.api.TreeRegistry;
+
 import com.ferreusveritas.dynamictrees.trees.Species;
 import com.ferreusveritas.dynamictrees.util.SafeChunkBounds;
 import net.dries007.tfc.api.types.Tree;
 import net.dries007.tfc.api.util.ITreeGenerator;
+import org.labellum.mc.dynamictreestfc.proxy.CommonProxy;
 
-import static com.ferreusveritas.dynamictrees.ModTrees.OAK;
 
 public class DTFCGenerator implements ITreeGenerator
 {
+
     @Override
     public void generateTree(TemplateManager templateManager, World world, BlockPos blockPos, Tree tree, Random random, boolean b)
     {
-        Species dtSpecies = TreeRegistry.findSpecies(new ResourceLocation(ModConstants.MODID, OAK));
+        Species dtSpecies = CommonProxy.treeMap.get(tree);
         dtSpecies.generate(world, blockPos.down(), world.getBiome(blockPos), random, 8, SafeChunkBounds.ANY);
         //getJoCode("JP").setCareful(true).generate(world, dtSpecies, blockPos, world.getBiome(blockPos), EnumFacing.SOUTH, 8, SafeChunkBounds.ANY);
     }
--- src/main/java/org/labellum/mc/dynamictreestfc/proxy/CommonProxy.java
+++ src/main/java/org/labellum/mc/dynamictreestfc/proxy/CommonProxy.java
@@ -1,33 +1,63 @@
 package org.labellum.mc.dynamictreestfc.proxy;
 
+import java.util.Map;
+
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
 import net.minecraft.block.Block;
 import net.minecraft.util.ResourceLocation;
 
+import com.ferreusveritas.dynamictrees.ModConstants;
 import com.ferreusveritas.dynamictrees.api.TreeRegistry;
 import com.ferreusveritas.dynamictrees.trees.Species;
 import net.dries007.tfc.api.registries.TFCRegistries;
 import net.dries007.tfc.api.types.Tree;
 import net.dries007.tfc.objects.blocks.BlocksTFC;
 import net.dries007.tfc.objects.blocks.stone.BlockRockVariant;
+import net.dries007.tfc.types.DefaultTrees;
 import org.labellum.mc.dynamictreestfc.DTFCGenerator;
+
+import static com.ferreusveritas.dynamictrees.ModTrees.*;
 
 public class CommonProxy
 {
-    public ImmutableList<BlockRockVariant> allGrowableVariants;
-    public ImmutableList<Species> allDTSpecies;
+    public static ImmutableList<BlockRockVariant> allGrowableVariants;
+    public static ImmutableList<Species> allDTSpecies;
+
+    public static Map<Tree, Species> treeMap;
+
     public void preInit() {
     }
 
     public void init() {
+        buildMaps();
         replaceGenerators();
     }
 
-    private void replaceGenerators() {
+    public void buildMaps()
+    {
+        ImmutableMap.Builder<Tree, Species> mapBuild = ImmutableMap.builder();
+        mapBuild.put(TFCRegistries.TREES.getValue(DefaultTrees.ACACIA), TreeRegistry.findSpecies(new ResourceLocation(ModConstants.MODID, ACACIA)));
+        mapBuild.put(TFCRegistries.TREES.getValue(DefaultTrees.ASH), TreeRegistry.findSpecies(new ResourceLocation(ModConstants.MODID, BIRCH)));
+        mapBuild.put(TFCRegistries.TREES.getValue(DefaultTrees.ASPEN), TreeRegistry.findSpecies(new ResourceLocation(ModConstants.MODID, BIRCH)));
+        mapBuild.put(TFCRegistries.TREES.getValue(DefaultTrees.BIRCH), TreeRegistry.findSpecies(new ResourceLocation(ModConstants.MODID, BIRCH)));
+        mapBuild.put(TFCRegistries.TREES.getValue(DefaultTrees.BLACKWOOD), TreeRegistry.findSpecies(new ResourceLocation(ModConstants.MODID, DARKOAK)));
+        mapBuild.put(TFCRegistries.TREES.getValue(DefaultTrees.CHESTNUT), TreeRegistry.findSpecies(new ResourceLocation(ModConstants.MODID, DARKOAK)));
+        mapBuild.put(TFCRegistries.TREES.getValue(DefaultTrees.DOUGLAS_FIR), TreeRegistry.findSpecies(new ResourceLocation(ModConstants.MODID, CONIFER)));
+        mapBuild.put(TFCRegistries.TREES.getValue(DefaultTrees.HICKORY), TreeRegistry.findSpecies(new ResourceLocation(ModConstants.MODID, DARKOAK)));
+        mapBuild.put(TFCRegistries.TREES.getValue(DefaultTrees.KAPOK), TreeRegistry.findSpecies(new ResourceLocation(ModConstants.MODID, JUNGLE)));
+        mapBuild.put(TFCRegistries.TREES.getValue(DefaultTrees.MAPLE), TreeRegistry.findSpecies(new ResourceLocation(ModConstants.MODID, OAK)));
+        mapBuild.put(TFCRegistries.TREES.getValue(DefaultTrees.OAK), TreeRegistry.findSpecies(new ResourceLocation(ModConstants.MODID, OAK)));
+        mapBuild.put(TFCRegistries.TREES.getValue(DefaultTrees.PALM), TreeRegistry.findSpecies(new ResourceLocation(ModConstants.MODID, JUNGLE)));
+        mapBuild.put(TFCRegistries.TREES.getValue(DefaultTrees.PINE), TreeRegistry.findSpecies(new ResourceLocation(ModConstants.MODID, CONIFER)));
+        mapBuild.put(TFCRegistries.TREES.getValue(DefaultTrees.ROSEWOOD), TreeRegistry.findSpecies(new ResourceLocation(ModConstants.MODID, OAK)));
+        mapBuild.put(TFCRegistries.TREES.getValue(DefaultTrees.SEQUOIA), TreeRegistry.findSpecies(new ResourceLocation(ModConstants.MODID, CONIFER)));
+        mapBuild.put(TFCRegistries.TREES.getValue(DefaultTrees.SPRUCE), TreeRegistry.findSpecies(new ResourceLocation(ModConstants.MODID, SPRUCE)));
+        mapBuild.put(TFCRegistries.TREES.getValue(DefaultTrees.SYCAMORE), TreeRegistry.findSpecies(new ResourceLocation(ModConstants.MODID, SPRUCE)));
+        mapBuild.put(TFCRegistries.TREES.getValue(DefaultTrees.WHITE_CEDAR), TreeRegistry.findSpecies(new ResourceLocation(ModConstants.MODID, CONIFER)));
+        mapBuild.put(TFCRegistries.TREES.getValue(DefaultTrees.WILLOW), TreeRegistry.findSpecies(new ResourceLocation(ModConstants.MODID, BIRCH)));
 
-        for (Tree tree: TFCRegistries.TREES.getValues()) {
-            tree.setTreeGenerator(new DTFCGenerator());
-        }
+        treeMap = mapBuild.build();
 
         ImmutableList.Builder<BlockRockVariant> rockBuild = ImmutableList.builder();
         for (BlockRockVariant rock : BlocksTFC.getAllBlockRockVariants())
@@ -43,6 +73,14 @@ public class CommonProxy
         {
             specBuild.add(TreeRegistry.findSpecies(loc));
             allDTSpecies = specBuild.build();
+        }
+
+    }
+
+    private void replaceGenerators() {
+
+        for (Tree tree: TFCRegistries.TREES.getValues()) {
+            tree.setTreeGenerator(new DTFCGenerator());
         }
 
         for (Species species : allDTSpecies) {
