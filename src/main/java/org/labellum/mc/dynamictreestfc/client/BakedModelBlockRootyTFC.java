9db2f954e3ed133a701862186f1efbc5ea36cb72
EERussianguy <eerussianguy@gmail.com>
2021-01-20 11:29:01 -0500

    fix #42

--- src/main/java/org/labellum/mc/dynamictreestfc/client/BakedModelBlockRootyTFC.java
+++ src/main/java/org/labellum/mc/dynamictreestfc/client/BakedModelBlockRootyTFC.java
@@ -2,7 +2,6 @@ package org.labellum.mc.dynamictreestfc.client;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 import javax.annotation.Nonnull;
 
 import net.minecraft.block.state.IBlockState;
@@ -11,15 +10,24 @@ import net.minecraft.client.renderer.BlockModelShapes;
 import net.minecraft.client.renderer.block.model.BakedQuad;
 import net.minecraft.client.renderer.block.model.IBakedModel;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.client.model.IModel;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.client.model.ModelLoaderRegistry;
 import net.minecraftforge.common.property.IExtendedBlockState;
 
 import com.ferreusveritas.dynamictrees.blocks.MimicProperty;
 import com.ferreusveritas.dynamictrees.models.bakedmodels.BakedModelBlockRooty;
 
+import static org.labellum.mc.dynamictreestfc.DynamicTreesTFC.MOD_ID;
+
 public class BakedModelBlockRootyTFC extends BakedModelBlockRooty
 {
     private TextureAtlasSprite particleSprite = Minecraft.getMinecraft().getTextureMapBlocks().getTextureExtry("minecraft:dirt");
+    private static final IModel rootyIModel = ModelLoaderRegistry.getModelOrMissing(new ResourceLocation(MOD_ID, "block/roots"));
+    private static final IBakedModel rootyIBakedModel = rootyIModel.bake(rootyIModel.getDefaultState(), DefaultVertexFormats.BLOCK, ModelLoader.defaultTextureGetter());
 
     public BakedModelBlockRootyTFC()
     {
@@ -39,6 +47,7 @@ public class BakedModelBlockRootyTFC extends BakedModelBlockRooty
             BlockModelShapes blockModelShapes = Minecraft.getMinecraft().getBlockRendererDispatcher().getBlockModelShapes();
             IBakedModel mimicModel = blockModelShapes.getModelForState(mimicState);
             quads.addAll(mimicModel.getQuads(mimicState, side, rand));
+            quads.addAll(rootyIBakedModel.getQuads(state, side, rand));
             if (!quads.isEmpty())
                 particleSprite = quads.get(0).getSprite();
         }
