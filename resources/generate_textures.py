22486152e3bfd351fa197f4eeb915a1849ddeece
metafive <40429600+m3t4f1v3@users.noreply.github.com>
2026-03-21 17:46:27 +1100

    some datagen stuff, still no worldgen, nor placing, need to fix a bunch of tags

--- resources/generate_textures.py
+++ resources/generate_textures.py
@@ -9,7 +9,7 @@ PATH = '../src/main/resources/assets/dttfc/textures/'
 def main():
     for dirt in assets.DIRT_TYPES:
         overlay_image('texture_templates/roots', 'texture_templates/mud/%s' % dirt, PATH + 'block/rooty_%s_mud' % dirt)
-        overlay_image('texture_templates/roots', 'texture_templates/dirt/%s' % dirt, PATH + 'block/rooty_%s_dirt' % dirt)
+        overlay_image('texture_templates/roots', 'texture_templates/rooted_dirt/%s' % dirt, PATH + 'block/rooty_%s_dirt' % dirt)
     for color in assets.SAND_COLORS:
         overlay_image('texture_templates/roots', 'texture_templates/sand/%s' % color, PATH + 'block/rooty_%s_sand' % color)
 
